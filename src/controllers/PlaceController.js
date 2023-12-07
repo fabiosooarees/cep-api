df5e2def5716907afbc0f740ed837b3b39c0cc88
Fabio <fabiosooarees@gmail.com>
2023-12-07 00:09:24 -0300

    test

--- src/controllers/PlaceController.js
+++ src/controllers/PlaceController.js
@@ -1,24 +1,20 @@
-const db = require('../database/connection')
+const db = require('../database/connection');
 
 class PlaceController {
+  async getByCEP(req, res) {
+    try {
+      const { cep } = req.params;
+      if (!cep) throw new Error('CEP informado é inválido');
 
-    async getByCEP(req, res){
-        try{
-            const { cep } = req.params
-            if(!cep)
-                throw new Error('CEP informado é inválido')
+      const sanitizedCep = cep.replace(/[^0-9]/g, '');
+      if (!sanitizedCep) throw new Error('CEP informado é inválido');
 
-            const sanitizedCep = cep.replace(/[^0-9]/g,'')
-            if(!sanitizedCep)
-            throw new Error('CEP informado é inválido')
-
-            const place = await db('places').select('*').where({cep: sanitizedCep})
-            res.status(200).json({place})
-        }catch(error){
-            res.status(500).json({error: error.message})
-        }
-        
+      const place = await db('places').select('*').where({ cep: sanitizedCep });
+      res.status(200).json({ place });
+    } catch (error) {
+      res.status(500).json({ error: error.message });
     }
+  }
 }
 
-module.exports = new PlaceController()
+module.exports = new PlaceController();
